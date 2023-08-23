ef83ad757ab38bc86de4274e9c6dc0c665f62993
Ahsan-bashir <bteeccc@gmail.com>
2023-08-23 06:45:26 +0500

    Implemented DELETE method for To-do Tracker

--- Week 1/Ceate REST API using VanillaJS(To-do Tracker)/app.js
+++ Week 1/Ceate REST API using VanillaJS(To-do Tracker)/app.js
@@ -18,6 +18,23 @@ const server = http.createServer(async (request, response) => {
         })
         response.end(JSON.stringify(JSON.parse(req_body)))
     }
+    else if(request.url.match('/\/api\/v1\/todos\/([0-9])/') && request.method==='DELETE'){
+        const id=request.url.split('/')[4]
+        const todo=todos.find(t=>t.id===parseInt(id))   
+        if(!todo){
+            response.writeHead(404,{
+                'content-type':'application/json'
+            })
+            response.end("No such Id is present !")
+        }else{
+            const index=todos.indexOf(todo)
+            todos.splice(index,1)
+            response.writeHead(200,{
+                'content-type':'application/json'
+            })
+            response.end('ID is Deleted')
+        }
+    }
 
 })
 
