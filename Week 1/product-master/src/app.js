2439c134e334bccf63a5e2d3d7d8947a07b93622
Ahsan-bashir <bteeccc@gmail.com>
2023-08-26 23:29:51 +0500

    Implemented and Updated DELETE and POST methods

--- Week 1/product-master/src/app.js
+++ Week 1/product-master/src/app.js
@@ -39,38 +39,116 @@ const server = http.createServer(async (req, res) => {
   }
   // Create a new product
 
+  // else if (req.url === '/api/v1/productsList' && req.method === 'POST') {
+  //   let req_body = ""
+  //   req_body = await getRequestData(req)
+  //   if (req_body) {
+  //     const isProduct=productsService.saveProduct(JSON.parse(req_body))
+  //     if (isProduct!==null) {
+       
+  //       res.writeHead(201, {
+  //         'content-type': 'application/json'
+  //       })
+  //       res.end(JSON.stringify(JSON.parse(req_body)))
+  //     } else {
+  //       res.writeHead(400, {
+  //         'Content-Type': 'application/json'
+  //       });
+  //       res.end("Product already exit !")
+  //     }
+  //   } else {
+  //     res.writeHead(400, {
+  //       'Content-Type': 'application/json'
+  //     });
+  //     res.end("Unable to retrieve data from request")
+  //   }
+  // }
+
+
   else if (req.url === '/api/v1/productsList' && req.method === 'POST') {
-    let req_body = ""
-    req_body = await getRequestData(req)
-    if (req_body) {
-      let isProduct = productsList.find(product => product.id === req_body.id)
-      if (!isProduct) {
-        productsList.push(JSON.parse(req_body))
-        res.writeHead(201, {
-          'content-type': 'application/json'
-        })
-        res.end(JSON.stringify(JSON.parse(req_body)))
-      } else {
-        res.writeHead(400, {
-          'Content-Type': 'application/json'
+    try {
+        const req_body = await getRequestData(req);
+
+        if (req_body) {
+            const productToAdd = JSON.parse(req_body);
+            productsService.saveProduct(productToAdd, (error, isProduct) => {
+                if (error) {
+                    res.writeHead(400, {
+                        'Content-Type': 'application/json'
+                    });
+                    res.end(JSON.stringify({ error: "Product already exists!" }));
+                } else {
+                    res.writeHead(201, {
+                        'Content-Type': 'application/json'
+                    });
+                    res.end(JSON.stringify(productToAdd));
+                }
+            });
+        } else {
+            res.writeHead(400, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({ error: "Unable to retrieve data from request" }));
+        }
+    } catch (error) {
+        res.writeHead(500, {
+            'Content-Type': 'application/json'
         });
-        res.end("Product already exit !")
-      }
-    } else {
-      res.writeHead(400, {
-        'Content-Type': 'application/json'
-      });
-      res.end("Unable to retrieve data from request")
+        res.end(JSON.stringify({ error: "Internal server error" }));
     }
-  }
+}
+
   // Update a specific product
   if(req.url.match('/\/api\/v1\/productsList\/([0-9])/') && req.method === 'PUT'){
     const id = parseInt(req.url.split('/')[4])
     const product = productsService.updateProduct(id)
-    
+    if(product){
+      res.writeHead(204,{
+        'content-type':'application/json'
+      })
+      res.end(JSON.stringify(product))
+    }else{
+      res.writeHead(404, {
+        'Content-Type': 'application/json'
+      });
+      res.end('Product not found');
+    }
 
   }
   // Delete a specific Product
+  // if(req.url.match('/\/api\/v1\/productsList\/([0-9])/') && req.method === 'DELETE'){
+  //   const id = parseInt(req.url.split('/')[4])
+  //   const product = productsService.deleteProduct(id)
+  //   if(product){
+  //     response.writeHead(200, {
+  //       'content-type': 'application/json'
+  //   })
+  //   response.end("Id is Deleted !")
+  //   }else{
+  //     response.writeHead(404, {
+  //       'content-type': 'application/json'
+  //   })
+  //   response.end("No such Id is present !")
+  //   }
+  // }
+
+
+  if (req.url.match(/\/api\/v1\/productsList\/([0-9])/) && req.method === 'DELETE') {
+    const id = parseInt(req.url.split('/')[4]);
+    productsService.deleteProduct(id, (error, product) => {
+        if (error) {
+            res.writeHead(404, {
+                'Content-Type': 'application/json'
+            });
+            res.end("No such Id is present !");
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end("Id is Deleted !");
+        }
+    });
+}
 
 });
 
