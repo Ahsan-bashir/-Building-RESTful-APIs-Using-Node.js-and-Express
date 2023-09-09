00c6d52e2ceda831743c12879010f9ad8793a4e8
Ahsan-bashir <bteeccc@gmail.com>
2023-09-09 15:22:23 +0500

    post request to register a user

--- Week 3/Authentication using JWT/Authentication/authRouter.js
+++ Week 3/Authentication using JWT/Authentication/authRouter.js
@@ -3,3 +3,26 @@ const express=require('express');
 const router=express.Router();
 const authController=require('./authController');
 
+// post request to register a user
+
+router.post('/register',(req,res)=>{
+    try {
+        const {name,email,password}=req.body;
+        if(!name || !email || !password){
+          return res.status(400).send({message:'Please provide all the details'});
+        }
+        const userDetails={
+            name,
+            email,
+            password
+        }
+        authController.registerUser(userDetails,(err,result)=>{
+            if(err){
+                return res.status(400).send({message:"User Alrady exists !!"});
+            }
+            return res.status(200).send({message:result});
+        })
+    } catch (error) {
+        return res.status(400).send({message:error.message});
+    }
+});
