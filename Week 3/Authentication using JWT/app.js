0d86bef1f46dff3f4be29a82369bb3ab285942e7
Ahsan-bashir <bteeccc@gmail.com>
2023-09-09 14:15:51 +0500

    parse JSON data into req.body

--- Week 3/Authentication using JWT/app.js
+++ Week 3/Authentication using JWT/app.js
@@ -1,10 +1,10 @@
 // Import express and other libraries
 const express=require('express');
 const app=express();
-const dateFormat=require('dateformat');
+const dateFormat=require('date-format');
 const authRouter=require('./Authentication');
 const confiq=require('./config');
-
 const morgan=require('morgan');
-
+// parse JSON data into req.body
+app.use(express.json());
 
