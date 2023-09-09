02a291cf14af4c116fdbf6312d72aa2b4a7fc929
Ahsan-bashir <bteeccc@gmail.com>
2023-09-09 15:38:19 +0500

    function to find and register a user

--- Week 3/Authentication using JWT/Authentication/authController.js
+++ Week 3/Authentication using JWT/Authentication/authController.js
@@ -1,3 +1,22 @@
 // import necessary modules
 const userService = require('../Users/userService');
+const { use } = require('./authRouter');
 const authService = require('./authService');
+
+// function to find and register a user
+
+function registerUser(userDetails, done) {
+    userService.findUser(userDetails.email, (err, userFound) => {
+        if (err) {
+            return done(err, null);
+        }else{
+            if(userFound){
+                return done(new Error('User already exists !!'),null);
+        }else{
+            userService.registerUser(userDetails,done)
+        }
+    }
+})
+}
+
+module.exports = {registerUser};
