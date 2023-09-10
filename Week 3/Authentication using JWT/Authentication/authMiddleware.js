8f864062c6628822bd043122d7f87940ab77118d
Ahsan-bashir <bteeccc@gmail.com>
2023-09-10 13:19:34 +0500

    middleware function to verify token

--- Week 3/Authentication using JWT/Authentication/authMiddleware.js
+++ Week 3/Authentication using JWT/Authentication/authMiddleware.js
@@ -1,2 +1,20 @@
 const JWT=require('jsonwebtoken');
 const config=require('../config');
+// middleware function to verify token
+function verifyToken(req,res,next){
+    
+        const token=req.headers['authorization']
+        if(!token){
+            return res.status(403).send({message:"Please provide token"});
+        }
+
+        try {
+            const decoded=JWT.verify(token,config.AUTH_secret);
+            req.claims=decoded;
+        } catch (error) {
+            res.status(401).send({message:"Invalid token"});
+        }
+        return next();
+}
+
+module.exports=verifyToken;
