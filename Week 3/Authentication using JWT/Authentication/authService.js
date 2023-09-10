8c30eaeb58da1c46eefef781f0fa6cbd1dcf50df
Ahsan-bashir <bteeccc@gmail.com>
2023-09-10 09:50:49 +0500

    function to verify a user

--- Week 3/Authentication using JWT/Authentication/authService.js
+++ Week 3/Authentication using JWT/Authentication/authService.js
@@ -1,3 +1,11 @@
 // imported jwt and config
 const JWT = require('jsonwebtoken');
 const config=require('../config');
+// function to verify a user
+function verifyUser({email,password},userData){
+    if(userData.email===email && userData.password===password){
+        return true;
+    }
+    return false;
+}
+
