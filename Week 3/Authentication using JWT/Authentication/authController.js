4e78708e45af205418714305b9f85af588bec4ba
Ahsan-bashir <bteeccc@gmail.com>
2023-09-10 08:35:13 +0500

    setting the env variable(AUTH_secret) for JWT

--- Week 3/Authentication using JWT/Authentication/authController.js
+++ Week 3/Authentication using JWT/Authentication/authController.js
@@ -1,6 +1,5 @@
 // import necessary modules
 const userService = require('../Users/userService');
-const { use } = require('./authRouter');
 const authService = require('./authService');
 
 // function to find and register a user
@@ -9,14 +8,15 @@ function registerUser(userDetails, done) {
     userService.findUser(userDetails.email, (err, userFound) => {
         if (err) {
             return done(err, null);
-        }else{
-            if(userFound){
-                return done(new Error('User already exists !!'),null);
-        }else{
-            userService.registerUser(userDetails,done)
+        } 
+        else {
+            if (userFound) {
+                return done(new Error('User already exists !!'), null);
+            } else {
+                userService.registerUser(userDetails, done)
+            }
         }
-    }
-})
+    })
 }
 
-module.exports = {registerUser};
+module.exports = { registerUser };
--- Week 3/Authentication using JWT/config.js
+++ Week 3/Authentication using JWT/config.js
@@ -1,5 +1,6 @@
 // config the PORT for the server to listen on
 const config={
     PORT:process.env.PORT || 3000,
+    AUTH_secret:process.env.AUTH_secret || 'secret' //setting the env variable(AUTH_secret) for JWT 
 }
 module.exports=config;  
