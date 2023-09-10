d8c769fd95bc479037d0ca6df4adea6af9111b11
Ahsan-bashir <bteeccc@gmail.com>
2023-09-10 09:40:31 +0500

    function to login a user for controller layer

--- Week 3/Authentication using JWT/Authentication/authController.js
+++ Week 3/Authentication using JWT/Authentication/authController.js
@@ -8,7 +8,7 @@ function registerUser(userDetails, done) {
     userService.findUser(userDetails.email, (err, userFound) => {
         if (err) {
             return done(err, null);
-        } 
+        }
         else {
             if (userFound) {
                 return done(new Error('User already exists !!'), null);
@@ -18,5 +18,24 @@ function registerUser(userDetails, done) {
         }
     })
 }
+// function to login a user for controller layer
+function loginUser({ email, password }, done) {
+    userService.findUser(email, (err, userFound) => {
+        if (err) {
+            return done(err, null);
+        }
+        else {
+            if (userFound) {
+                const userVerified = authService.verifyUser({ email, password }, userFound);
+                if (userVerified) {
+                    const jwtToken = authService.createJWT(userFound)
+                    return done(null, { message: 'User Logged in Successfully !!', jwtToken });
+                } else {
+                    return done(new Error('user not verified !!'), null);
+                }
+            }
+        }
+    })
+}
 
-module.exports = { registerUser };
+module.exports = { registerUser,loginUser };
