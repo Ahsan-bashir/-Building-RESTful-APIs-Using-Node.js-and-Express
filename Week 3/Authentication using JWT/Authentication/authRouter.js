140f07cb7f5be78dde85e7d90ec5368286fd0617
Ahsan-bashir <bteeccc@gmail.com>
2023-09-10 08:41:30 +0500

    post request to login a user

--- Week 3/Authentication using JWT/Authentication/authRouter.js
+++ Week 3/Authentication using JWT/Authentication/authRouter.js
@@ -26,5 +26,25 @@ router.post('/register',(req,res)=>{
         return res.status(400).send({message:error.message});
     }
 });
-
+// post request to login a user
+router.post('/login',(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).send({message:"Please provide all the details"});
+        }
+        const userDetails={
+            email,
+            password
+        }
+        authController.loginUser(userDetails,(err,result)=>{
+            if(err){
+                return res.status(401).send({message:"Invalid Credentials "});
+            }
+            return res.status(200).send({message:result});
+        })
+    } catch (error) {
+        return res.status(400).send({message:"Unexpected error while logging the user",error});
+    }
+})
 module.exports=router;
