e573960ebf99223392be344fdf6b7e900314b8c3
Ahsan-bashir <bteeccc@gmail.com>
2023-09-13 12:38:52 +0500

    getMovies,getMoviesById,saveMovie done

--- Week 1/movie-application-master/src/moviesService.js
+++ Week 1/movie-application-master/src/moviesService.js
@@ -1,27 +1,41 @@
 // Import the axios library
 const axios = require('axios')
+const movies = require('../data/movies.json')
 
 const getMovies = (done) => {
   // get all movies
-  axios.get(url)
-  .then((response)=>{
-      c  
-  })
-  .catch((error)=>{
-
+  axios.get('http://localhost:3000/movies').then((response) => {
+    done(response.data)
+  }).catch((error) => {
+    done(error)
   })
 }
 
 const getMoviesById = (movieId, done) => {
   // get movie by id
+  axios.get('http://localhost:3000/movies/' + movieId).then((response) => {
+    done(response.data)
+  }).catch((error) => {
+    done(error)
+  })
 }
 
 const saveMovie = function (newMovie, done) {
   // save the details of a movie read from the request body
+  axios.post('http://localhost:3000/movies', newMovie)
+  .then((response) => {
+    const movie = response.data
+    movies.push(movie)
+    done(movies)
+  })
+  .catch((error) => {
+    done(error)
+  })
 }
 
 const updateMovie = function (movieId, updateData, done) {
- // update movie details of a specific movie
+  // update movie details of a specific movie
+  axios.post('')
 }
 
 const deleteMovieById = function (movieId, done) {
