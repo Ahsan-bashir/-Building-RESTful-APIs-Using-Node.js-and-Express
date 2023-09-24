4349d46cb77bee870f1ec4add9753ad7adf30b8f
Ahsan-bashir <bteeccc@gmail.com>
2023-09-24 12:53:31 +0500

    implemented all controllers

--- Week 2/movie-application-master/src/moviecontroller.js
+++ Week 2/movie-application-master/src/moviecontroller.js
@@ -1,29 +1,84 @@
 //import service layer
-
+const movieservice = require("./movieservice");
 
 const getMovies = (done) => {
 //call service method getMovies method
- 
+ movieservice.getMovies((err, results) => {
+      if(err){
+       return res.status(500).json({
+          message: "Error while fetching movies"
+        })
+      }
+      return res.status(200).json({
+        message: "Movies fetched successfully",
+        data: results     
+    })
+    }
+    );
 }
 
 const getMovieById = (movieId, done) => {
     //call service method getMovieById method
- 
+ movieservice.getMovieById(movieId, (err, results) => {
+      if(err){
+       return res.status(500).json({
+          message: "Error while fetching movies"
+        })
+      }
+      return res.status(200).json({
+        message: "Movies fetched successfully",
+        data: results     
+    })
+    }
+    );
 }
 
 const saveMovieDetails = (movieDetails, done) => {
   //call service method saveMovieDetails method
-  
+  movieservice.saveMovieDetails(movieDetails, (err, results) => {
+      if(err){
+       return res.status(500).json({
+          message: "Error while fetching movies"
+        })
+      }
+      return res.status(200).json({
+        message: "Movies fetched successfully",
+        data: results     
+    })
+    }
+    );
 }
 
 const updateMovieDetails = (movieId, movieDetails, done) => {
   //call service method updateMovieDetails method
- 
+ movieservice.updateMovieDetails(movieId, movieDetails, (err, results) => {
+      if(err){
+       return res.status(500).json({
+          message: "Error while fetching movies"
+        })
+      }
+      return res.status(200).json({
+        message: "Movies fetched successfully",
+        data: results     
+    })
+    }
+    );
 }
 
 const deleteMovieById = (movieId, done) => {
   //call service method deleteMovieById method
-  
+  movieservice.deleteMovieById(movieId, (err, results) => { 
+      if(err){
+       return res.status(500).json({
+          message: "Error while fetching movies"
+        })
+      }
+      return res.status(200).json({
+        message: "Movies fetched successfully",
+        data: results     
+    })
+    }
+    );
 }
 
 module.exports = {
