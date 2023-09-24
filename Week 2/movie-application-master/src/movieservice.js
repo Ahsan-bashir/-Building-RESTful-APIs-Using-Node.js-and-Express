93378544a17957103f11f6b5ed3d63352c249b80
Ahsan-bashir <bteeccc@gmail.com>
2023-09-24 11:58:26 +0500

    implemented axios for all get methods

--- Week 2/movie-application-master/src/movieservice.js
+++ Week 2/movie-application-master/src/movieservice.js
@@ -1,35 +1,61 @@
 
 //import axios module
-
+const axios = require("axios");
 //After starting the JSOn server check the port on which is running accordingly change 
 //the port in url given below
-
+const PORT=process.env.PORT || 3000;
 //This method will get all movies from json server
 const getMovies = (done) => {
   // This url can be used - axios.get("http://localhost:3000/movies")
- 
+  axios.get(`http://localhost:${PORT}/movies`).then((response) => {
+    done(null, response.data);
+  }
+  ).catch((err) => {
+    done(err, null);
+  }
+  )
 }
 
 //This method will get specific movie id from json server
 const getMovieById = (movieId, done) => {
-  // This url can be used- axios.get(`http://localhost:3000/movies/${movieId}`)
+   axios.get(`http://localhost:${PORT}/movies/${movieId}`).then((response) => {
+    done(null, response.data);
+}).catch((err) => {
+    done(err, null);
+})
  
 }
 //This method will save Movie details in Json server
 const saveMovieDetails = (movieDetails, done) => {
-  //This url can be used  -  axios.post(`http://localhost:3000/movies`, movieDetails)
+   axios.post(`http://localhost:${PORT}/movies`, movieDetails).then((response) => {
+    done(null, response.data);
+}).catch((err) => {
+    done(err, null);
+
+})
  
 }
 
 //This method will update MovieDetails in Json Server
 const updateMovieDetails = (movieId, movieDetails, done) => {
-  //This url can be used - axios.patch(`http://localhost:3000/movies/${movieId}`, movieDetails)
+ axios.patch(`http://localhost:${PORT}/movies/${movieId}`, movieDetails).then((response) => {
+    done(null, response.data);  
+}).catch((err) => {
+    done(err, null);
+
+})
  
 }
 
 //This method will delete specific movie from Json Server
 const deleteMovieById = (movieId, done) => {
-  //This url can be used -  axios.delete(`http://localhost:3000/movies/${movieId}`)
+    axios.delete(`http://localhost:${PORT}/movies/${movieId}`).then((response) => {
+    done(null, response.data);
+}).catch((err) => {
+    done(err, null);
+
+
+})
  
 }
 
