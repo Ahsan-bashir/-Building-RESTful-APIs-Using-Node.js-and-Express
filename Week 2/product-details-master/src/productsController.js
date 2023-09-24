8162f46bf2e357c90a28be4f19eb9f537cbd1610
Ahsan-bashir <bteeccc@gmail.com>
2023-09-24 14:55:56 +0500

    implemented Controllers+Services Layers

--- Week 2/product-details-master/src/productsController.js
+++ Week 2/product-details-master/src/productsController.js
@@ -1,27 +1,76 @@
 
 
 //import the productService
-
+const productService = require('./productsService');
 
 const getProducts = (done) => {
    //call service getproducts method and pass the parameter
+productService.getProducts((err, results) => {
+      if(err){
+        return res.status(400).json({
+          status: "error",
+          message: err.message
+        })
+      }
+      return res.status(200).json({
+          status: "success",
+          data: results
+        })
+    }
+)
+}   
 
-}
 
 const getProductById = (productId, done) => {
    //call service getProductById method and pass the parameter
-
+productService.getProductById(productId, (err, results) => {
+      if(err){
+        return res.status(400).json({
+          status: "error",
+          message: err.message
+        })
+      }
+      return res.status(200).json({
+          status: "success",
+          data: results
+        })
+    } 
+)
 }
 
 const saveProductDetails = (productDetails, done) => {
   //call service saveProductDetails method and pass the parameter
-
+productService.saveProductDetails(productDetails, (err, results) => {
+      if(err){
+        return res.status(400).json({
+          status: "error",
+          message: err.message
+        })
+      }
+      return res.status(200).json({
+          status: "success",
+          data: results
+        })
+    }
+)
 }
 
 
  const deleteProductById = (productId, done) => {
    //call service deleteProductById method and pass the parameter
-  
+  productService.deleteProductById(productId, (err, results) => {
+      if(err){
+        return res.status(400).json({
+          status: "error",
+          message: err.message
+        })
+      }
+      return res.status(200).json({
+          status: "success",
+          data: results
+        })
+    }
+)
  }
 
 module.exports = {
--- Week 2/product-details-master/src/productsService.js
+++ Week 2/product-details-master/src/productsService.js
@@ -1,25 +1,74 @@
 
 
 //import the DAO layer
-
+const productsDao = require('./productDao');
 
 const getProducts = function(done){
   //call dao getproducts method and pass the parameter
-  
+  productsDao.getProducts((err, results) => {
+    if(err){
+      return res.status(400).json({
+        status: "error",
+        message: err.message
+      })
+    }
+    return res.status(200).json({
+        status: "success",
+        data: results
+      })
+  }
+)
 }
 
 const getProductById = function(id, done){
   //call dao getProductById method and pass the parameter
- 
+ productsDao.getProductById(id, (err, results) => {
+    if(err){
+      return res.status(400).json({
+        status: "error",
+        message: err.message
+      })
+    }
+    return res.status(200).json({
+        status: "success",
+        data: results
+      })
+  }
+)
 }
 const saveProductDetails = function(productDetails, done){
   //call dao saveProductDetails method and pass the parameter
-
+productsDao.saveProductDetails(productDetails, (err, results) => {
+    if(err){
+      return res.status(400).json({
+        status: "error",
+        message: err.message
+      })
+    }
+    return res.status(200).json({
+        status: "success",
+        data: results
+      })
+  }
+)
 }
 
 
 const deleteProductById = (productId, done) => {
 //call dao deleteProductById method and pass the parameter
+productsDao.deleteProductById(productId, (err, results) => {
+    if(err){
+      return res.status(400).json({
+        status: "error",
+        message: err.message
+      })
+    }
+    return res.status(200).json({
+        status: "success",
+        data: results
+      })
+  }
+)
 }
 
 
