bd256c80b64071fcf8c44421230cd99f347665f8
Ahsan-bashir <bteeccc@gmail.com>
2023-09-29 22:30:58 +0500

    code to get access token from github+installed axios

---
+++ Week 3/GitHub Authentication/oauth/auth_services.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const config = require('../config');
+
+// code to get access token from github
+
+const getGitHubAccessToken=(code,done)=>{
+    const body={
+        client_id:config.CLIENT_ID,
+        client_secret:config.CLIENT_SECRET,
+        code:code
+    }
+    const opts={headers:{accept:'application/json'}}
+
+    axios.post('https://github.com/login/oauth/access_token',body,opts).then((response)=>{
+        response.data.access_token
+    }).then((token)=>{
+        done(null,token);
+    }).catch((err)=>{
+        done(err,null);
+    })
+}
+
+module.exports=getGitHubAccessToken;
