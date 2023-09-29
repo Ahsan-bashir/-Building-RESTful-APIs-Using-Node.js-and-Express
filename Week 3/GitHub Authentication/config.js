bb6c3fa68d13f71bcc3251225bf97c31e42c9800
Ahsan-bashir <bteeccc@gmail.com>
2023-09-29 22:14:21 +0500

    Added client ID and Secret+Updated callback URL path

--- Week 3/GitHub Authentication/config.js
+++ Week 3/GitHub Authentication/config.js
@@ -1,5 +1,5 @@
 const config={
      PORT:process.env.PORT|| "3000",
-     CLIENT_ID:process.env.CLIENT_ID||,
-     CLIENT_SECRET:process.env.CLIENT_SECRET||
-}
+     CLIENT_ID:process.env.CLIENT_ID|| "436a7d32d46dfedcfc86",
+     CLIENT_SECRET:process.env.CLIENT_SECRET||"a6c698dab0a41a4371f578ba0326b2ae35a62347"
+} 
