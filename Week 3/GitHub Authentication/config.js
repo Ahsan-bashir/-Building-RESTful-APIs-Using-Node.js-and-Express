d6c6b6c4428aef0b85eee5f6570c610f641fba38
Ahsan-bashir <bteeccc@gmail.com>
2023-09-29 22:20:19 +0500

    export configurations

--- Week 3/GitHub Authentication/config.js
+++ Week 3/GitHub Authentication/config.js
@@ -3,3 +3,4 @@ const config={
      CLIENT_ID:process.env.CLIENT_ID|| "436a7d32d46dfedcfc86",
      CLIENT_SECRET:process.env.CLIENT_SECRET||"a6c698dab0a41a4371f578ba0326b2ae35a62347"
 } 
+module.exports=config;
