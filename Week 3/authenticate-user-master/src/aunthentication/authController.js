87702b8d8c924d7eb9d769e5e132040065c1f79c
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:39:16 +0500

    Implemented authController layer

--- Week 3/authenticate-user-master/src/aunthentication/authController.js
+++ Week 3/authenticate-user-master/src/aunthentication/authController.js
@@ -1,19 +1,56 @@
 
 
 //import the userService and authService module
+const authService=require('./authService');
+const userService=require('../Users/userService');
 
 //This function will registerUser it will take two parameters
 //first the userData second the callback
 //call the userService finduser method and also the userService register method
 function registerUser(userData,done){
-    
+    userService.findUser(userData.email,(err,user)=>{
+        if(err){
+            done(err,null)
+        }
+        else if(user){
+            done({message:'User already exists'},null)
+        }
+        else{
+            userService.registerUser(userData,(err,result)=>{
+                if(err){
+                    done(err,null)
+                }
+                else{
+                    done(null,result)
+                }
+            })
+        }
+    }
+    )
 }
 
 //This function will loginUser 
 //Use the method findUser of userService to first verify and if userfound than call
 //the createToken method
 function loginUser({ email, password }, done) {
-  
+    userService.findUser(email, (err, user) => {
+        if (err) {
+            done(err, null)
+        }
+        else if (!user) {
+            done({ message: 'User does not exist' }, null)
+        }
+        else {
+            authService.createToken(user, password, (err, result) => {
+                if (err) {
+                    done(err, null)
+                }
+                else {
+                    done(null, result)
+                }
+            })
+        }
+    })  
   }
 
 module.exports = {
