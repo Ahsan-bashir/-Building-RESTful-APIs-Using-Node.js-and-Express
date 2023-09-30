ef3dfc62f43bea3568cd7cbc74e9f6191040e1b3
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:49:36 +0500

    Implemented userServices

--- Week 3/authenticate-user-master/src/Users/userController.js
+++ Week 3/authenticate-user-master/src/Users/userController.js
@@ -1,11 +1,18 @@
 
 
 //import userService
-
+const userService=require('./userService');
 
 function findUser(email,done){
     //call userService findUser method and pass the parameters
-   
+   userService.findUser(email,(err,result)=>{
+       if(err){
+           done(err,null)
+       }
+       else{
+           done(null,result)
+       }
+   })
 }
 
 module.exports = {
