2ecae994e6ee51a1dc8d9db6c91bcffca5a67b72
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:51:15 +0500

    implemented userService

--- Week 3/authenticate-user-master/src/Users/userService.js
+++ Week 3/authenticate-user-master/src/Users/userService.js
@@ -1,14 +1,29 @@
 
 
 //import dao layer
+const userDao = require('./userDAO');
 function findUser(email,done){
     //call the userdao finduser method
-    
+    userDao.findUser(email,(err,result)=>{
+        if(err){
+            done(err,null)
+        }
+        else{
+            done(null,result)
+        }
+    })
 }
 
 function registerUser(userData,done){
     //call the userdao registeruser method
-   
+   userDao.registerUser(userData,(err,result)=>{
+       if(err){
+           done(err,null)
+       }
+       else{
+           done(null,result)
+       }
+   })
 }
 
 
