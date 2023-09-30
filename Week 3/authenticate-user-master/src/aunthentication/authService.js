00a8eede72283fa6608b00d93706cb75fea861db
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:42:21 +0500

    Implemented authServices

--- Week 3/authenticate-user-master/src/aunthentication/authService.js
+++ Week 3/authenticate-user-master/src/aunthentication/authService.js
@@ -1,12 +1,11 @@
 
 
 //import jsonwebtoken and config file
-
+const JWT=require('jsonwebtoken');
+const config=require('../../config');
 //This function will verify email and password and will return true and false
 
 function verifyUser({email,password},userData){
- 
-  
    if(userData===undefined){
   return false
    }
@@ -15,7 +14,6 @@ function verifyUser({email,password},userData){
      if(email === userData.email && password === userData)
      return true;
    }
-    
   
 }
 
@@ -23,7 +21,13 @@ function verifyUser({email,password},userData){
 // use the method jwt.sign having two parameters payload and Auth_Secret
 function createJWT(userdata) {
   //create payload
-   
+   const payload = {
+      email: userdata.email,
+      password: userdata.password,
+    };
+    //create token
+    const token = JWT.sign(payload, config.AUTH_SECRET);
+    //return token
     return token;
   }
 
