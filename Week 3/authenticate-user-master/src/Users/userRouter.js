2616a3fcd25ae2a284858adfd62129d5634d2245
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:48:28 +0500

    implememnted userRouter

--- Week 3/authenticate-user-master/src/Users/userRouter.js
+++ Week 3/authenticate-user-master/src/Users/userRouter.js
@@ -1,17 +1,22 @@
 
 
 //import the required module
-
+const express=require('express');
+const router=express.Router();
+const userController=require('./userController');
 //This get method will get the user with token
-router.get('/',(req,res)=>{
-    
-       //retrive userdata from req claims
-       
-
-        //Calling controller findUser method return the error or result
-        userController.findUser(userdata.email,(err,result)=>{
-           
-        })
+router.get('/',(req,res)=>{    
+//retrive userdata from req claims
+        const userdata=req.claims;
+//Calling controller findUser method return the error or result
+  userController.findUser(userdata.email,(err,result)=>{
+     if(err){
+         res.status(500).json({message:err.message})
+     }
+     else{
+         res.status(200).json({message:result})
+     }      
+})
    
 })
 
