872a9c1783a28371c2de0d4bbbb8970701cc4de0
Ahsan-bashir <bteeccc@gmail.com>
2023-09-30 23:22:17 +0500

    Implemented middleware for authentication

--- Week 3/authenticate-user-master/src/aunthentication/authMiddleware.js
+++ Week 3/authenticate-user-master/src/aunthentication/authMiddleware.js
@@ -1,15 +1,27 @@
 
 
 //import jsonwebtoken and config
-
+const JWT=require('jsonwebtoken');
+const config=require('../../config');
 //This function verifyToken will verify the token coming from headers 
 const verifyToken = (req, res, next) => {
   // Getting the authorization header
   const token = req.headers["authorization"];
-
+  //if token is not present then return error message
+  if (!token) return res.status(403).json({ error: "Forbidden" });
+  //if token is present then verify it
+  JWT.verify(token, config.secret, (err, decoded) => {
+    //if error is present then return error message
+    if (err) return res.status(401).json({ error: "Unauthorized" });
+    //if decoded is present then set req.user to decoded
+    req.user = decoded;
+  });
  
 //Synchronously verify given token using a secret or a public key to get a decoded token 
- 
+  const decoded = JWT.verify(token, config.secret);
+  //set req.user to decoded
+  req.user = decoded;
+  
   //return next
   return next();
 };
